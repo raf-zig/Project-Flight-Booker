dcb6926c427cb847b883b55efb32483e741acfdd
raf-zig <ziravin@yandex.ru>
2023-09-07 16:45:04 +0500

    Add search form on index page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all.map{ |u| [u.code, u.id]}
+    @flights = Flight.all.map{ |u| [u.departure_date, u.id]}
   end
 end
