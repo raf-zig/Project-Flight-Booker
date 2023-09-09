fc1b47e9e41ca91ea824971a8eadedeec50ca4c6
raf-zig <ziravin@yandex.ru>
2023-09-10 00:57:01 +0500

    Add display of available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map{ |u| [u.code, u.id]}
-    @flights = Flight.all.map{ |u| [u.departure_date, u.id]}
+    @flights = Flight.all.map{ |u| [u.departure_date, u.departure_date] }
+    @available_flights = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND  departure_date = ?",
+    params[:departure_airport_id], params[:arrival_airport_id], params[:departure_date])
   end
 end
