e5667e3ddec39dc54451e27f87e21d9700ae5a7c
raf-zig <ziravin@yandex.ru>
2023-09-19 14:04:53 +0500

    Change nested form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,16 @@ class BookingsController < ApplicationController
   def new
     @number_of_passengers = params[:num_pass]
     @selected_flight = params[:id]
-    @pas = Passenger.new
+    #@pas = Passenger.new
     @book = Booking.new
+    @dep_time = Flight.find(@selected_flight).departure_time
+    @dep_airport = Flight.find(@selected_flight).departure_airport_id
+    @arr_airport = Flight.find(@selected_flight).arrival_airport_id
+    @da = Airport.find(@dep_airport).code
+    @aa = Airport.find(@arr_airport).code
+  end
+
+  def create
+
   end
 end
